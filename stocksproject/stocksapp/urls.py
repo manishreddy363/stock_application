f28fdce64f38aaf9fc3fee546658c90a8d1f04f2
kbabu <karadla.kondababu1@gmail.com>
2024-01-18 13:36:59 +0530

    added stock list page

--- stocksproject/stocksapp/urls.py
+++ stocksproject/stocksapp/urls.py
@@ -1,8 +1,10 @@
 # stocksapp/urls.py
 from django.urls import path
-from .views import login_view, hello_world
+from .views import login_view, hello_world, stock_search, stock_detail
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('search_stock/', hello_world, name='hello_world'),
+    path('stock_search/', stock_search, name='stock_search'),
+    path('stock-detail/<str:stock>/', stock_detail, name='stock_detail'),
 ]
--- stocksproject/stocksapp/views.py
+++ stocksproject/stocksapp/views.py
@@ -23,3 +23,29 @@ def login_view(request):
             messages.error(request, 'Invalid username or password.')
 
     return render(request, 'login.html')
+
+
+def stock_search(request):
+    # Dummy list of stocks (replace this with your actual stock data)
+    stocks = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'FB', 'NVDA', 'NFLX', 'PYPL', 'INTC',
+              'AQPL', 'GOWGL', 'MSFE', 'AMRN', 'TLA', 'FSB', 'NWDA', 'ANFLX', 'SPYPL', 'EINTC']
+
+    # Get the search query from the request
+    query = request.GET.get('search', '')
+
+    # Filter stocks based on the search query
+    matching_stocks = [stock for stock in stocks if stock.lower().startswith(query.lower())]
+    # matching_stocks = [stock for stock in stocks if query.lower() in stock.lower()]
+    matching_stocks.sort()
+
+    return render(request, 'stock_search.html', {'stocks': matching_stocks, 'query': query})
+
+
+def stock_detail(request, stock):
+    # Retrieve stock details based on the stock symbol
+    # (You need to implement this view)
+    # For example, you might query a database for the details of the selected stock.
+    # stock_details = get_stock_details(stock)
+    
+    # Render the stock detail page with the details
+    return render(request, 'stock_detail.html', {'stock': stock})
