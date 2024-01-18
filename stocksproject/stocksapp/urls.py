ee44ae3c6d630d153563f4dab9df9aa1b43d147d
manishreddy363 <manishreddy363@gmail.com>
2024-01-18 15:23:35 +0530

    commit changes

--- stocksproject/stocksapp/urls.py
+++ stocksproject/stocksapp/urls.py
@@ -4,7 +4,7 @@ from .views import login_view, hello_world, stock_list, stock_detail
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('hello_world/', hello_world, name='hello_world'),
+    # path('hello_world/', hello_world, name='hello_world'),
     path('stock_search/', stock_list, name='stock_list'),
     path('stock-detail/<str:stock>/', stock_detail, name='stock_detail'),
 ]
