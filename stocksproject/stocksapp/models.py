770954ebf238bb0238e884937b52da7b525826f2
manishreddy363 <manishreddy363@gmail.com>
2024-01-19 14:02:01 +0530

    2nd page api

--- stocksproject/stocksapp/models.py
+++ stocksproject/stocksapp/models.py
@@ -9,11 +9,11 @@ class CustomUser(AbstractUser):
 
 
 class Stock_ID(models.Model):
-    stock_name = models.CharField(max_length=30)
+    stock_name = models.CharField(max_length=30,unique=True)
     stock_id = models.IntegerField(unique=True)
 
     def __str__(self):
-        return str(self.stock_name)
+        return str(self.stock_name,self.stock_id)
 
 
 class Stock_Details_Table(models.Model):
@@ -22,7 +22,7 @@ class Stock_Details_Table(models.Model):
     Stock_Details_Description = models.TextField(max_length=250)
     Stock_Details_Country = models.CharField(max_length=30)
     Stock_Details_State = models.CharField(max_length=30)
-    Stock_Details_StockID = models.ForeignKey(Stock_ID, on_delete=models.CASCADE,to_field='stock_id')
+    Stock_Details_StockID = models.ForeignKey(Stock_ID, on_delete=models.CASCADE,to_field='stock_id',unique=True)
     Stock_Details_Sector = models.CharField(max_length=30)
     Stock_Details_Industry = models.CharField(max_length=30)
     Stock_Details_Total_Revenue = models.CharField(max_length=30)
@@ -51,8 +51,9 @@ class Stock_Naics_Table(models.Model):
 class stock_historical_data_v3(models.Model):
     Stock_Historical_Data_V3_Column1 = models.IntegerField()
     Stock_Historical_Data_V3_Year = models.IntegerField()
+    Stock_Historical_Data_V3_Quarter = models.IntegerField(blank=True,default=11)
     Stock_Historical_Data_V3_StockID = models.ForeignKey(Stock_ID, on_delete=models.CASCADE)
-    Stock_Historical_Data_V3_FilingDate = models.IntegerField()
+    Stock_Historical_Data_V3_FilingDate = models.CharField(max_length=30)
     Stock_Historical_Data_V3_FiscalQuarterEnd = models.CharField(max_length=30)
     Stock_Historical_Data_V3_FQE_10 = models.CharField(max_length=30)
     Stock_Historical_Data_V3_Price_Next_Day = models.FloatField()
@@ -69,7 +70,7 @@ class stock_Earnings(models.Model):
 
     Stock_Earnings_Estimated_EPS = models.FloatField()
     Stock_Earnings_Actual_EPS = models.FloatField()
-    Stock_Earnings_StockID = models.IntegerField()
+    Stock_Earnings_StockID =  models.ForeignKey(Stock_ID, on_delete=models.CASCADE)
     Stock_Earnings_Surprise = models.FloatField()
 
     Stock_Earnings_End_of_Quarter = models.CharField(max_length=30)
@@ -83,7 +84,7 @@ class stock_Earnings(models.Model):
 
 class Next_filing_dates(models.Model):
     Next_Filing_Dates_Column1 = models.IntegerField()
-    Next_Filing_Dates_StockID = models.IntegerField()
+    Next_Filing_Dates_StockID =  models.ForeignKey(Stock_ID, on_delete=models.CASCADE)
     Next_Filing_Dates_Avg_Diff = models.IntegerField()
     Next_Filing_Dates_DateDifference = models.IntegerField()
     Next_Filing_Dates_FiscalQuarter = models.IntegerField()
--- stocksproject/stocksapp/views.py
+++ stocksproject/stocksapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Stock_ID
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Stock_ID, Stock_Details_Table
+from django.http import JsonResponse, HttpResponse
+
+# @login_required(login_url='/admin/login/')
+def hello_world(request):
+    return render(request, 'hello_world.html', {'user': request.user})
 
 
 @login_required(login_url='/')
@@ -26,7 +34,54 @@ def stock_analysis(request, stock):
     # Retrieve stock details based on the stock symbol
     # (You need to implement this view)
     # For example, you might query a database for the details of the selected stock.
-    # stock_details = get_stock_details(stock)
-    
-    # Render the stock detail page with the details
-    return render(request, 'stock_detail_analysis.html', {'stock': stock})
+    """
+    stock_analysis_data = {
+    "stock_id_name":"ACP.to"
+    "stock_name":"",
+    "stock_description":"",
+    "stock_revenue":{"county":"","provins":"","revenue":29984},
+    "naics_code":[{},{},{}],
+    "fiscal_data":[{},{},{}],
+    "eps_data":[{},{},{}],
+    "next_period":[{},{},{}],
+    "filing_date":""
+    }
+    """
+    print(stock)
+    stock_analysis_data ={}
+    # revenue by geography
+    try:
+        stock_id = Stock_ID.objects.get(stock_name=stock).stock_id
+        stock_data = Stock_Details_Table.objects.get(Stock_Details_StockID=stock_id)
+        print(stock_data)
+        stock_analysis_data["stock_id_name"] = stock
+        stock_analysis_data["stock_name"] = stock_data.Stock_Details_Name
+        stock_analysis_data["description"] = stock_data.Stock_Details_Description
+        stock_revenue_dict = {}
+        stock_revenue_dict['Country'] = stock_data.Stock_Details_Country
+        stock_revenue_dict['Province'] = stock_data.Stock_Details_State
+        stock_revenue_dict['Revenue(TTM)'] = stock_data.Stock_Details_Total_Revenue
+
+        stock_analysis_data["stock_revenue"] = stock_revenue_dict
+    except:
+        print("invalid stock id")
+        messages.error(request, 'Invalid {} stock name'.formate(stock))
+
+    #NAICS code
+    stock_analysis_data['naics_code'] = [{'code':'',"level":"","class_title":""}]
+
+    #stock price & impact (table & chart)
+    stock_analysis_data['fiscal_data'] = [{'filing_date':'','fiscal_quarter_end':'','stock_price':'','next_day_stock_price':'','impact':''}]
+
+    #EPS table
+    stock_analysis_data['eps_data'] = [{'filing_date':'','fiscal_quarter_end':'','forecasted_eps':'',
+                                        'ei_eps':'', 'actual_eps':'','f_delta':'','ei_delta':''}]
+
+    #next period
+    stock_analysis_data['next_period'] = [{'next_filing_date':'','fiscal_quarter_end':'','forecasted_eps':'',
+                                        'ei_eps':'', 'actual_eps':''}]
+
+    # Trading days remaining before next filing date
+    stock_analysis_data["filing_date"] = 230
+
+    return render(request, 'stock_detail_analysis.html', {'stock_analysis_data': stock_analysis_data})
